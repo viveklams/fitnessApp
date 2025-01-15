fbd8354e0a399999232eff3e683db80e21925c38
viveklams <viveklamgadey@gmail.com>
2025-01-15 16:33:57 +0530

    created workoutRoutes routes by using router() given by express and on the server.js we are calling that creating like a restful APIs

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,16 @@ const configureDb = require("./config/db");
 const app = express();
 const PORT = 3001;
 
+//routes
+const workoutRoutes = require("./routes/workoutRoutes");
+
 configureDb();
 
 app.use(express.json());
 app.use(cors()); // Use the cors middleware
+
+//route
+app.use("/api", workoutRoutes);
 
 app.listen(PORT, () => {
   console.log("server runnin on port", PORT);
