beff866b73fb9c8948943b3c4a57d405eabc54bb
viveklams <viveklamgadey@gmail.com>
2025-01-15 16:31:07 +0530

    created a workout routes for backend

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -1,2 +1,8 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+
+const workoutController = require("../controllers/workoutController");
+
+router.post("/workouts", workoutController.createWorkout);
+
+module.exports = router;
